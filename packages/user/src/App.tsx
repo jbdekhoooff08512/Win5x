ef921e57f4e981a639fef8cf2bb1bb7e871a4aa6
jebdekho53 <jebdekho@gmail.com>
2025-10-02 19:08:17 +0530

    Add landing page to user application routing

--- packages/user/src/App.tsx
+++ packages/user/src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster } from 'react-hot-toast';
 import { useAuth } from './contexts/AuthContext';
 import { BettingWarningProvider } from './contexts/BettingWarningContext';
 import Layout from './components/Layout';
+import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import GamePage from './pages/GamePage';
@@ -47,9 +48,10 @@ function App() {
   if (!user) {
     return (
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     );
   }
